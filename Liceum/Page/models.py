bf973d0f03ee73c9a5eadff7ea866fa5608d65b8
Martolod Slaaf <mart.slaaf@gmail.com>
2012-07-03 13:51:21 +0400

    pages

--- Liceum/Page/models.py
+++ Liceum/Page/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib import admin
 from Menu.models import Menu
 
-# Create your models here.
-
 class Page(models.Model):
 	name = models.CharField(max_length=30)
 	text = models.TextField(null=True,blank=True)
@@ -11,6 +9,7 @@ class Page(models.Model):
 	menupoint = models.ForeignKey(Menu,on_delete=models.SET_NULL, null=True)
 	title = models.CharField(null=True,blank=True,max_length=30)
 	position = models.IntegerField(null=True,blank=True)
+	innerpage = models.BooleanField()
 	
 class PageAdmin(admin.ModelAdmin):
     list_display = ('name','visible')
